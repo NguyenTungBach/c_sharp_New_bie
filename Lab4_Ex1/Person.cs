3639e8a06d11283d27a98b8b3bb42ac64bad1ec2
Nguyen Tung Bach <bachntth2010055@fpt.edu.vn>
2021-09-26 10:54:51 +0700

    lab4 and lab 5

---
+++ Lab4_Ex1/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab4_Ex1
+{
+    public interface Person
+    {
+        string name { get; set; }
+        string phone { get; set; }
+        string email { get; set; }
+        
+    }
+}
